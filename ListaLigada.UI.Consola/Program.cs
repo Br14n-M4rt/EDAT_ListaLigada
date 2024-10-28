96a33e5effae90d4fb9fd069fb6295d411b12ebc
Br14n-M4rt <23030256@itesa.edu.mx>
2024-10-28 09:17:20 -0600

    Versión_03: Se agrego el menú de opciones.

--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ListaLigada.Logica;
+using System.ComponentModel.Design;
 
 internal class Program
 {
@@ -9,14 +10,66 @@ internal class Program
 
         var singleList = new SingleList<string>();
 
-        singleList.Add("Alondra");
-        singleList.Add("Karla");
-        singleList.Add("Jan");
-        singleList.Add("Luis");
-        singleList.Add("Jose");
+        int opcion = 0;
+        do
+        {
+            opcion = Menu();
 
-        Console.WriteLine(singleList);
+            switch (opcion)
+            {
+                case 1: AgregarElemento();  break;
+                case 2: MostrarLista(); break;
+                case 0: break;
+
+                    default: Console.WriteLine("Esta opcion no existe"); break;
+            }
+        }
+        while (opcion != 0);
+
+        void AgregarElemento()
+        {
+            Console.WriteLine("Escribe el nombre del nodo\n");
+            var data = Console.ReadLine();
+            singleList.Add(data);
+        }
+
+        void MostrarLista()
+        {
+            Console.WriteLine("La lista es:\n");
+            Console.WriteLine(singleList);
+        }
 
         Console.ReadKey();
+    }//TERMINA EL MAIN
+
+    private static int Menu()
+    {
+        Console.WriteLine("\nMenu");
+        Console.WriteLine("1. Agregar elementon");
+        Console.WriteLine("2. Mostrar Lista");
+        Console.WriteLine("0. Salir\n");
+
+        bool esValido = false;
+        
+        int opcion = 0;
+
+        do
+        {
+            Console.Write("Elige una opcion del menu: ");
+            var opcionCapturada = Console.ReadLine();
+
+            if (!int.TryParse(opcionCapturada, out opcion) || opcion < 0 || opcion > 2)
+            {
+                Console.WriteLine("Digite una opcion valida");
+                esValido = false;
+            }
+            else
+            {
+                esValido = true;
+            }
+        }
+        while (esValido == false);
+
+        return opcion;
     }
 }
